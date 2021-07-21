f8af481e659f663a017735c969b417f7ef184952
Bangtx <bangtran.hha@gmail.com>
2021-07-21 21:17:51 +0700

    12

--- main.py
+++ main.py
@@ -18,13 +18,8 @@ def get_db():
         db.close()
 
 
-@app.post("/users/", response_model=User)
+@app.post("/users/")
 def create_user(user: User, db: Session = Depends(get_db)):
-    # db_user = crud.get_user_by_email(db, email=user.email)
-    # if db_user:
-    #     raise HTTPException(status_code=400, detail="Email already registered")
-    print('xxx')
-    print(user, db)
     db_user = models.UserModel(id=user.id, email=user.email, password=user.password)
     db.add(db_user)
     db.commit()
